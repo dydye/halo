44815d262b519525269092ad957e0d42e9155ae6
daiyun <daiyun@tuzhanai.com>
2019-12-24 17:04:07 +0800

    change

--- src/main/java/run/halo/app/model/entity/PostTag.java
+++ src/main/java/run/halo/app/model/entity/PostTag.java
@@ -1,7 +1,6 @@
 package run.halo.app.model.entity;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 import javax.persistence.*;
@@ -19,40 +18,40 @@ import java.util.Objects;
 @ToString(callSuper = true)
 public class PostTag extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
 
-    /**
-     * Post id.
-     */
-    @Column(name = "post_id", columnDefinition = "int not null")
-    private Integer postId;
+	/**
+	 * Post id.
+	 */
+	@Column(name = "post_id", columnDefinition = "int not null")
+	private Integer postId;
 
-    /**
-     * Tag id.
-     */
-    @Column(name = "tag_id", columnDefinition = "int not null")
-    private Integer tagId;
+	/**
+	 * Tag id.
+	 */
+	@Column(name = "tag_id", columnDefinition = "int not null")
+	private Integer tagId;
 
-    @Override
-    public void prePersist() {
-        super.prePersist();
-        id = null;
-    }
+	@Override
+	public void prePersist() {
+		super.prePersist();
+		id = null;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PostTag postTag = (PostTag) o;
-        return Objects.equals(postId, postTag.postId) &&
-                Objects.equals(tagId, postTag.tagId);
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		PostTag postTag = (PostTag) o;
+		return Objects.equals(postId, postTag.postId) &&
+				Objects.equals(tagId, postTag.tagId);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(postId, tagId);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(postId, tagId);
+	}
 }
